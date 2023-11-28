6478cc3afbd721d6b4bdcb6282145c162553b65a
PedroSmaxY <pedrohnovais@outlook.com>
2023-11-28 00:41:05 -0300

    Fix capitalization in region name

--- src/main_compras.c
+++ src/main_compras.c
@@ -70,7 +70,7 @@ int main()
     printf("\n----------------------------------------\n");
     printf("Resumo da Compra");
     printf("\n----------------------------------------\n");
-    printf("Região: %s", compra.regiao == 1 ? "norte" 
+    printf("Região: %s", compra.regiao == 1 ? "Norte" 
     : compra.regiao == 2 ? "Nordeste"
     : compra.regiao == 3 ? "Sul" 
     : "Sudeste");
