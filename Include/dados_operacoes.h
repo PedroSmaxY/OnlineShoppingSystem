cbc761ff79ba2c8f4c909c315f6d60665f8a1485
PedroSmaxY <pedrohnovais@outlook.com>
2023-11-30 00:06:15 -0300

    Refactor calcularFrete function and add new functions for displaying product list and purchase summary

--- Include/dados_operacoes.h
+++ Include/dados_operacoes.h
@@ -48,12 +48,12 @@ void limparConsole()
 }
 
 // Função para calcular o preço do frete
-float calcularFrete(Compra compra)
+void calcularFrete(Compra *compra)
 {
     float fretePadrao, fretePeso;
 
     // Calcula os valores do frete com base na região
-    switch (compra.regiao)
+    switch (compra->regiao)
     {
     case 1: // Norte
         fretePadrao = 35.00;
@@ -72,37 +72,43 @@ float calcularFrete(Compra compra)
         fretePeso = 45.00;
         break;
     default:
-        printf("Regiao invalida\n");
-        return -1; // Retorna -1 se a região for inválida
+        printf("Regiao invalida\n"); // Retorna -1 se a região for inválida
         break;
     }
 
     // Retorna o valor do frete com base no peso
-    for (int i = 0; i < compra.quantidadeProdutos; i++)
+    for (int i = 0; i < compra->quantidadeProdutos; i++)
     {
-        if (compra.produtos[i].peso > 2)
+        if (compra->produtos[i].peso > 2)
         {
-            return fretePeso;
+            compra->precoFrete = fretePeso;
+            break;
+        }
+        else
+        {
+            compra->precoFrete = fretePadrao;
         }
     }
-    return fretePadrao;
+    compra->precoTotal += compra->precoFrete;
 }
 
 // Função para exibir a lista de produtos disponíveis
 void exibirListaProdutos(Produto listaProdutos[], int tamanho)
 {
     printf("\n----------------------------------------\n");
+    printf("Sistema de Compras");
+    printf("\n----------------------------------------\n");
     printf("Lista de produtos disponiveis:");
     printf("\n----------------------------------------\n");
     for (int i = 0; i < tamanho; i++)
     {
-        printf("%d - %s - %.02fkg - R$%.2f\n", i + 1, listaProdutos[i].nome, listaProdutos[i].peso, listaProdutos[i].preco);
+        printf("%d - %s - %.02fKg - R$%.2f\n", i + 1, listaProdutos[i].nome, listaProdutos[i].peso, listaProdutos[i].preco);
     }
     printf("\n----------------------------------------\n");
 }
 
 // Função para solicitar a região ao usuário
-int solicitarRegiao()
+void solicitarRegiao(Compra *compra)
 {
     int regiao;
     printf("\n----------------------------------------\n");
@@ -131,11 +137,11 @@ int solicitarRegiao()
     {
         limparConsole();
         printf("Regiao invalida. Tente novamente.\n");
-        return solicitarRegiao(); // Chama recursivamente se a região for inválida
+        return solicitarRegiao(compra); // Chama recursivamente se a região for inválida
     }
     else
     {
-        return regiao;
+        compra->regiao = regiao;
     }
 }
 
@@ -187,6 +193,57 @@ void selecionarProdutos(Compra *compra, Produto listaProdutos[], int tamanho)
         }
         limparBufferEntrada();
     }
+}
+
+// Função para obter a data e hora atuais
+void dataHora(Compra *compra)
+{
+    // Obtém a data e hora atuais
+    time_t t;
+    struct tm *data;
+    t = time(NULL);
+    data = localtime(&t);
+
+    // Formata a data e hora da compra
+    sprintf(compra->dataHoraCompra, "%02d/%02d/%d %02d:%02d", data->tm_mday, data->tm_mon + 1, data->tm_year + 1900, data->tm_hour, data->tm_min);
+
+    // Adiciona 7 dias à data atual para obter a previsão de chegada
+    data->tm_mday += 7;
+    mktime(data);
+
+    // Formata a previsão de chegada
+    sprintf(compra->previsaoChegada, "%02d/%02d/%d", data->tm_mday, data->tm_mon + 1, data->tm_year + 1900);
+}
+
+// Função para exibir o resumo da compra
+void ResumoDaCompra(Compra *compra)
+{
+    dataHora(compra);
+    printf("\n----------------------------------------\n");
+    printf("Resumo da Compra");
+    printf("\n----------------------------------------\n");
+    printf("Regiao: %s", compra->regiao == 1 ? "Norte" 
+    : compra->regiao == 2 ? "Nordeste"
+    : compra->regiao == 3 ? "Sul" 
+    : "Sudeste");
+
+    printf("\n----------------------------------------\n");
+    printf("Produtos selecionados:\n\n");
+
+    // Exibe os produtos selecionados
+    for (int i = 0; i < compra->quantidadeProdutos; i++)
+    {
+        printf("id:%d - %s - %.2fKg - R$%.2f\n", compra->produtos[i].codigo,compra->produtos[i].nome, compra->produtos[i].peso, compra->produtos[i].preco);
+    }
+    printf("----------------------------------------\n");
+    printf("Valor dos produtos: R$%.2f", compra->precoProdutos);
+    printf("\n----------------------------------------\n");
+    printf("Valor do frete: R$%.2f", compra->precoFrete);
+    printf("\nValor total: R$%.2f", compra->precoTotal);
+    printf("\n----------------------------------------\n");
+    printf("Data e hora da compra: %s\n", compra->dataHoraCompra);
+    printf("Previsao de chegada: %s", compra->previsaoChegada);
+    printf("\n----------------------------------------\n");
 }
 
 // Finaliza o pré-processamento condicional
